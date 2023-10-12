2fcca30c0b6963c8b88f2db8d4d654c46215897e
Guille <gcortesmoya@gmail.com>
2023-10-12 00:37:39 -0300

    set workflow

---
+++ script/create_db.sh
@@ -0,0 +1,11 @@
+NAME=$1
+DB_VERSION=$2
+DB_SIZE=$3
+
+FILE_PATH=deploy/${NAME}-db.yaml
+
+cp crossplane-claim/db.yaml $FILE_PATH
+yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
+yq --inplace ".spec.id = \"${NAME}\"" $FILE_PATH
+yq --inplace ".spec.parameters.db.version = \"${DB_VERSION}\"" $FILE_PATH
+yq --inplace ".spec.parameters.db.size = ${MIN_SIZE}" $FILE_PATH
